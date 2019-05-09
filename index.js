77f3b6309b3616b0fb536c8a9556a0c586b97a3b
nishantprajapti <nishuprajapati92@gmail.com>
2019-05-09 18:35:18 +0530

    checking files

--- index.js
+++ index.js
@@ -1,29 +1,58 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var path = require("path");
+var express = require('express');
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+var mongo = require('mongodb');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/connectYou";
+
+
+// MIDDLEWARE //
+app.use(express.static(path.join(__dirname, '../dist')));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + './dist/index.html');
+});
+
+app.get('/home', function (req, res) {
+  res.sendFile((path.join(__dirname, '../dist')) +  '/question.html');
+});
+
+app.use("/login", function (req, res, next) {
+  let credentials = req.query;
+  if (!credentials || credentials.name !== 'harryanddavid' || credentials.pass !== 'harryanddavid@123') {
+    res.statusCode = 401;
+    res.end('Access denied')
+  } else {
+
+  }
+});
+
+app.use("/registration", function (req, res, next) {
+  let userData = req.query;
+  MongoClient.connect(url,
+    function (err, db) {
+      if (err) throw err;
+      console.log("Database connected!");
+      var dbo = db.db("connectYou");
+
+      dbo.collection("Users").insertOne(userData, function (err, res) {
+        if (err) {
+          throw err;
+        } else {
+          db.close();
+        }
+      });
+    });
+
+  res.send("success");
+  res.end('Success');
+
 });
 
 
-io.on('connection', function(socket){
-  // console.log('a user connected');
-  // socket.on('disconnect', function(){
-  //   console.log('user disconnected');
-  // });
-  // socket.on('chat message', function(msg){
-  //   console.log('message: ' + msg);
-  // });
-  // io.on('connection', function(socket){
-  //   socket.broadcast.emit('hi');
-  // });
-  // socket.broadcast.emit('hi');
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
-
-http.listen(3000, function(){
+http.listen(3000, function () {
   console.log('listening on *:3000');
 });
